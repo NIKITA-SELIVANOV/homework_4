eac43b4ac75ba2d5788de2cdaeae0bfbdf6787d9
NIKITA-SELIVANOV <nikitaselivano@rambler.ru>
2023-02-14 01:51:49 +0300

    task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+if (x<0){
+    x*=-1;
+}
+while (x>0){
+    sum +=x%10;
+    x/=10;
+}
+Console.WriteLine ((sum));
