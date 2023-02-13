556ca44487b5865b5b27ee109a3c5e73800cb711
NIKITA-SELIVANOV <nikitaselivano@rambler.ru>
2023-02-14 02:14:32 +0300

    task_25_1

---
+++ Task_25_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("ВВедите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите степень числа: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int square = x;
+
+ for (int i =1;i<z; i++){
+x = x*square;
+}
+Console.WriteLine ((x));
