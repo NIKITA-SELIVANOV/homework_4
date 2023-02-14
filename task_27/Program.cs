21a4441956c76bcd84355aefc881e138627bcab5
NIKITA-SELIVANOV <nikitaselivano@rambler.ru>
2023-02-14 09:22:18 +0300

    task_29

--- task_27/Program.cs
+++ task_27/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.WriteLine ("Введите число: ");
 int x = Convert.ToInt32(Console.ReadLine());
+
 int sum = 0;
 if (x<0){
     x*=-1;
@@ -8,4 +9,6 @@ while (x>0){
     sum +=x%10;
     x/=10;
 }
-Console.WriteLine ((sum));
+Console.WriteLine((sum));
+
+
--- task_29/Program.cs
+++ task_29/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int [] GenerateArray(){
+int [] arr = new int [8];
+for (int i =0;i<arr.Length; i++){
+arr[i] = new Random().Next();
+}
+
+return arr;
+}
+void PrintArray (int[] arr){
+    for (int i=0;i<arr.Length;i++){
+        Console.Write($"{arr[i]} ");
+    }
+
+}
+int[] array = GenerateArray();
+PrintArray(array);
